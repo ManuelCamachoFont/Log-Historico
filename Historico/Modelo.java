1405cef473459e3dd38f6720f6d435025eba0123
Manuel_Camacho_Font <manuelcamachofont@gmail.com>
2026-04-07 13:02:56 +0200

    ficheros en escritorio mensuales

--- Historico/Modelo.java
+++ Historico/Modelo.java
@@ -1,11 +1,13 @@
 package es.studium.Historico;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import javax.swing.filechooser.FileSystemView;
 
 public class Modelo {
 
@@ -19,7 +21,64 @@ public class Modelo {
 	public void guardar(String textoFormateado) {
 
 		try {
-			FileWriter fw = new FileWriter("historico.txt", true);
+			LocalDateTime timestamp = LocalDateTime.now();
+			DateTimeFormatter formatoFichero = DateTimeFormatter.ofPattern("dd_MM_yyyy");
+			DateTimeFormatter formatoDirectorio = DateTimeFormatter.ofPattern("MM_yyyy");
+			String nombreDirectorio = timestamp.format(formatoDirectorio);
+
+			switch (nombreDirectorio) {
+			case ("01_2026"):
+				nombreDirectorio = "Enero";
+				break;
+			case ("02_2026"):
+				nombreDirectorio = "Febrero";
+				break;
+			case ("03_2026"):
+				nombreDirectorio = "Marzo";
+				break;
+			case ("04_2026"):
+				nombreDirectorio = "Abril";
+				break;
+			case ("05_2026"):
+				nombreDirectorio = "Mayo";
+				break;
+			case ("06_2026"):
+				nombreDirectorio = "Junio";
+				break;
+			case ("07_2026"):
+				nombreDirectorio = "Julio";
+				break;
+			case ("08_2026"):
+				nombreDirectorio = "Agosto";
+				break;
+			case ("09_2026"):
+				nombreDirectorio = "Septiembre";
+				break;
+			case ("10_2026"):
+				nombreDirectorio = "Octubre";
+				break;
+			case ("11_2026"):
+				nombreDirectorio = "Noviembre";
+				break;
+			case ("12_2026"):
+				nombreDirectorio = "Diciembre";
+				break;
+			}
+			String nombreFichero = timestamp.format(formatoFichero).concat(".txt");
+
+			File escritorio = FileSystemView.getFileSystemView().getHomeDirectory();
+
+			File carpetaLog = new File(escritorio, "Logs");
+
+			File carpetaMes = new File(carpetaLog, nombreDirectorio);
+
+			if (!carpetaMes.exists()) {
+				carpetaMes.mkdirs();
+			}
+
+			File archivoTxt = new File(carpetaMes, nombreFichero);
+
+			FileWriter fw = new FileWriter(archivoTxt, true);
 			BufferedWriter bw = new BufferedWriter(fw);
 			PrintWriter salida = new PrintWriter(bw);
 			salida.println(textoFormateado);
